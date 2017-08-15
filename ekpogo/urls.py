a3ccd8028375689a26248f269d2043c62138661d
Alfie "Azelphur" Day <support@azelphur.com>
2017-08-15 20:18:07 +0100

    Initial Commit

---
+++ ekpogo/urls.py
@@ -0,0 +1,26 @@
+"""ekpogo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+api_v01_patterns = [
+    url(r"^enrollment/", include('enrollment.urls', namespace="enrollment")),
+]
+
+urlpatterns = [
+    url(r"^api/0.1/", include(api_v01_patterns, namespace="api_v01")),
+    url(r'^admin/', admin.site.urls),
+]
