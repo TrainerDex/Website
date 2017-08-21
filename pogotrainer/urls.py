30dcc426f3d627c27a07b28684f185b2f8bddbb2
Jay Turner <jaynicholasturner@gmail.com>
2017-08-21 23:20:10 +0100

    API-ification

--- pogotrainer/urls.py
+++ pogotrainer/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from api.resources import *
+
+levels_resource = Trainer_Levels_Resource()
+trainers_resource = Trainers_Resource()
+xp_resource = Experience_Resource()
+discordUsers_resource = Discordian_Resource()
+discordMembers_resource = Discordian_On_Servers_Resource()
+discordServers_resource = Servers_Resource()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'api/', include(trainers_resource.urls)),
 ]
