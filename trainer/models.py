b10a3a6ec276842bd29994822edb40b2b0176775
Jay Turner <jaynicholasturner@gmail.com>
2017-09-06 13:41:18 +0100

    Didn't reinvent the wheel
    Made use of `django.contrib.auth.models` `user` instead of creating my own usertable

--- trainer/models.py
+++ trainer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 def factionImagePath(instance, filename):
 	return os.path.join('factionLogo', str(instance.id), filename)
@@ -6,20 +7,10 @@ def factionImagePath(instance, filename):
 def leaderImagePath(instance, filename):
 	return os.path.join('factionLeader', str(instance.id), filename)
 
-
-#Login Models
-
-class Directory_Users(models.Model):
-	email = models.EmailField()
-	username = models.CharField(max_length=100)
-	password = models.CharField(max_length=50)
-
 class Trainer(models.Model):
-	account = models.ForeignKey('Directory_Users', on_delete=models.SET_NULL, null=True, blank=True)
 	username = models.CharField(max_length=30, unique=True)
-	discord = models.ForeignKey('Discord_Users', on_delete=models.SET_NULL, null=True, blank=True) 
 	start_date = models.DateField(null=True, blank=True)
-	faction = models.ForeignKey('Factions', on_delete=models.SET_DEFAULT, default=0)
+	faction = models.ForeignKey('Faction', on_delete=models.SET_DEFAULT, default=0)
 	join_date = models.DateField(auto_now_add=True)
 	has_cheated = models.BooleanField(default=False)
 	last_cheated = models.DateField(null=True, blank=True)
@@ -28,20 +19,24 @@ class Trainer(models.Model):
 	daily_goal = models.IntegerField(null=True, blank=True)
 	total_goal = models.IntegerField(null=True, blank=True)
 	last_modified = models.DateTimeField(auto_now=True)
+	prefered = models.BooleanField(default=True)
+	#Third Party Accounts
+	discord = models.ForeignKey('Discord_User', on_delete=models.SET_NULL, null=True, blank=True)
+	ekpogo = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
-class Factions(models.Model):
+class Faction(models.Model):
 	faction_name = models.CharField(max_length=140)
 	faction_colour = models.IntegerField(null=True, blank=True)
 	faction_image = models.ImageField(upload_to=factionImagePath, blank=True, null=True)
 	leader_name = models.CharField(max_length=140, null=True, blank=True)
 	leader_image = models.ImageField(upload_to=leaderImagePath, blank=True, null=True)
 
-class Trainer_Levels(models.Model):
+class Trainer_Level(models.Model):
 	level = models.AutoField(primary_key=True)
 	min_total_xp = models.IntegerField(blank=True, null=False)
 	relative_xp = models.IntegerField()
 
-class Experience(models.Model):
+class Update(models.Model):
 	trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE)
 	datetime = models.DateTimeField(auto_now_add=True)
 	xp = models.IntegerField(verbose_name='Total XP')
@@ -85,29 +80,28 @@ class Experience(models.Model):
 	#Other stats
 	gym_badges = models.IntegerField(null=True, blank=True)
 
-class Discord_Users(models.Model):
-	account = models.ForeignKey('Directory_Users', on_delete=models.SET_NULL, null=True, blank=True)
+class Discord_User(models.Model):
+	account = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 	d_name = models.CharField(max_length=32)
 	d_id = models.CharField(max_length=256, primary_key=True)
 	d_discriminator = models.CharField(max_length=256)
 	d_avatar_url = models.URLField()
 	d_creation = models.DateTimeField()
 	dob = models.DateField(null=True, blank=True)
-	real_name = models.CharField(max_length=256, null=True, blank=True)
 
-class Discord_Servers(models.Model):
+class Discord_Server(models.Model):
 	s_name = models.CharField(max_length=256)
 	s_region = models.CharField(max_length=256)
 	s_id = models.CharField(max_length=256, primary_key=True)
 	s_icon = models.CharField(max_length=256)
-	s_owner = models.ForeignKey('Discord_Users', on_delete=models.SET_NULL, null=True, blank=True)
+	s_owner = models.ForeignKey('Discord_User', on_delete=models.SET_NULL, null=True, blank=True)
 	bans_cheaters = models.BooleanField(default=True)
 	seg_cheaters = models.BooleanField(default=False)
 	bans_minors = models.BooleanField(default=False)
 	seg_minors = models.BooleanField(default=False)
 
-class Discord_Relations(models.Model):
-	d_id = models.ForeignKey('Discord_Users', on_delete=models.CASCADE)
-	d_server = models.ForeignKey('Discord_Servers', on_delete=models.CASCADE)
+class Discord_Relation(models.Model):
+	d_id = models.ForeignKey('Discord_User', on_delete=models.CASCADE)
+	d_server = models.ForeignKey('Discord_Server', on_delete=models.CASCADE)
 	verified = models.BooleanField(default=False)
 	ban = models.BooleanField(default=False)
--- trainer/views.py
+++ trainer/views.py
@@ -5,20 +5,20 @@ from rest_framework import status
 from .models import *
 from .serializers import *
 
-class Factions_List(APIView):
+class Faction_List(APIView):
 	
 	def get(self, request):
-		factions = Factions.objects.all()
-		serializer = Factions_Serializer(factions, many=True)
+		factions = Faction.objects.all()
+		serializer = Faction_Serializer(factions, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer = Factions_Serializer(data=request.data)
+		serializer = Faction_Serializer(data=request.data)
 		if  serializer.is_valid():
 			return Response(serializer.errors, status=status.HTTP_423_LOCKED) 
 		return Response(serializer.errors, status=status.HTTP_423_LOCKED)
 
-class Trainers_List(APIView):
+class Trainer_List(APIView):
 	
 	def get(self, request):
 		trainers = Trainer.objects.all()
@@ -35,63 +35,63 @@ class Trainers_List(APIView):
 class Level_List(APIView):
 	
 	def get(self, request):
-		levels = Trainer_Levels.objects.all()
-		serializer = Trainer_Levels_Serializer(levels, many=True)
+		levels = Trainer_Level.objects.all()
+		serializer = Trainer_Level_Serializer(levels, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer = Trainer_Levels_Serializer(data=request.data)
+		serializer = Trainer_Level_Serializer(data=request.data)
 		if  serializer.is_valid():
 			return Response(serializer.errors, status=status.HTTP_423_LOCKED) 
 		return Response(serializer.errors, status=status.HTTP_423_LOCKED)
 
-class Experience_List(APIView):
+class Update_List(APIView):
 	
 	def get(self, request):
-		experience = Experience.objects.all()
-		serializer = Experience_Serializer(experience, many=True)
+		experience = Update.objects.all()
+		serializer = Update_Serializer(experience, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer = Experience_Serializer(data=request.data)
+		serializer = Update_Serializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class Discord_Users_List(APIView):
+class Discord_User_List(APIView):
 	
 	def get(self, request):
-		discord = Discord_Users.objects.all()
-		serializer = Discord_Users_Serializer(discord, many=True)
+		discord = Discord_User.objects.all()
+		serializer = Discord_User_Serializer(discord, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer = Discord_Users_Serializer(data=request.data)
+		serializer = Discord_User_Serializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class Discord_Servers_List(APIView):
+class Discord_Server_List(APIView):
 	
 	def get(self, request):
-		server = Discord_Servers.objects.all()
-		serializer = Discord_Servers_Serializer(server, many=True)
+		server = Discord_Server.objects.all()
+		serializer = Discord_Server_Serializer(server, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
-		serializer = Discord_Servers_Serializer(data=request.data)
+		serializer = Discord_Server_Serializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class Discord_Relations_List(APIView):
+class Discord_Relation_List(APIView):
 	
 	def get(self, request):
-		donserver = Discord_Relations.objects.all()
-		serializer = Discord_Relations_Serializer(donserver, many=True)
+		donserver = Discord_Relation.objects.all()
+		serializer = Discord_Relation_Serializer(donserver, many=True)
 		return Response(serializer.data)
 	
 	def post(self, request, format=None):
