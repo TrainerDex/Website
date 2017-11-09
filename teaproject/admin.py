cfb5761e85c91c08c686e8a479ad30ae2b50713c
Jay Turner <jaynicholasturner@gmail.com>
2017-11-09 17:45:31 +0000

    Initial commit

---
+++ teaproject/admin.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from ajax_select import make_ajax_form
+from ajax_select.admin import AjaxSelectAdmin
+from ajax_select.fields import autoselect_fields_check_can_add
+from teaproject.models import *
+
+admin.site.register(Tea)
+admin.site.register(Sweetener)
+admin.site.register(Colour)
+
+@admin.register(Cuppa)
+class CuppaAdmin(AjaxSelectAdmin):
+	
+	form = make_ajax_form(Cuppa, {
+		'drinker': 'drinker'
+	})
+	list_display = ('drinker', 'tea', 'colour', 'iswaterhard', 'isbought')
+	search_fields = ('drinker__username', 'tea__name')
+	ordering = ('-datetime',)
