2090559221c23695e3b2d8883fb1cadffe68f824
Jay Turner <jaynicholasturner@gmail.com>
2017-11-12 15:41:46 +0000

    Changed how URLS are versioned alongside eachother
    Subject to change

--- ekpogo/urls.py
+++ ekpogo/urls.py
@@ -22,18 +22,18 @@ from ajax_select import urls as ajax_select_urls
 api_v0_1_patterns = [
     url(r"^enrollment/", include('enrollment.urls', namespace="enrollment")),
     url(r"^gyms/", include('gyms.urls', namespace="gyms")),
-    url(r"", include('trainer.0_1.urls', namespace="trainerdex")),
+    url(r"", include('trainer.urls.0.1', namespace="trainerdex")),
 ]
 
 api_v0_2_patterns = [
-    url(r"", include('trainer.urls', namespace="trainerdex")),
+    url(r"", include('trainer.urls.1.0', namespace="trainerdex")),
 ]
 
 urlpatterns = [
     url(r"^api/0.1/", include(api_v0_1_patterns, namespace="api_v01")),
     url(r"^api/0.2/", include(api_v0_2_patterns, namespace="api_v02")),
     url(r"^api/admin/", admin.site.urls),
-    url(r"^api/trainer/", include('trainer.0_1.urls', namespace="trainer")), # legacy url scheme
+    url(r"^api/trainer/", include('trainer.urls.0.1', namespace="trainer")), # legacy url scheme
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^ajax_select/', include(ajax_select_urls)),
     url(r'^accounts/', include('allauth.urls')),
