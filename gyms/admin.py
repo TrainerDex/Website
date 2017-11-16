d8003ee50d0a37bb7d93824f88a99910d62a6c0a
Jay Turner <jaynicholasturner@gmail.com>
2017-11-16 23:38:42 +0000

    Use django-material for admin

--- gyms/admin.py
+++ gyms/admin.py
@@ -1,4 +1,7 @@
 from django.contrib.gis import admin
 from .models import Town
-# Register your models here.
-admin.site.register(Town, admin.OSMGeoAdmin)
+
+
+@admin.register(Town)
+class UpdateAdmin(admin.OSMGeoAdmin):
+	icon = '<i class="material-icons">location_on</i>'
