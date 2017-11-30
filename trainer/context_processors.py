1535cf1ea34bbd8addcd5ab68d3b171be7d76790
Jay Turner <jaynicholasturner@gmail.com>
2017-11-30 12:07:52 +0000

    Fix bug with Analytics

--- trainer/context_processors.py
+++ trainer/context_processors.py
@@ -8,8 +8,8 @@ def google_analytics(request):
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
     ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
     if not settings.DEBUG and ga_prop_id and ga_domain:
-    return {
-        'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
-        'GOOGLE_ANALYTICS_DOMAIN': ga_domain,
-    }
+        return {
+            'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
+            'GOOGLE_ANALYTICS_DOMAIN': ga_domain,
+        }
     return {}
