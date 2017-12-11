0aed30be0cfcf3797d9f4b2078d1bf411c86b9de
Jay Turner <jaynicholasturner@gmail.com>
2017-12-11 01:14:26 +0000

    Listing basic data

--- trainer/views.py
+++ trainer/views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.decorators import detail_route
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.models import User
-from trainer.models import *
+from trainer.models import Trainer, Faction, Update, DiscordGuild
 from allauth.socialaccount.models import SocialAccount
-from trainer.serializers import *
+from trainer.serializers import UserSerializer, TrainerSerializer, FactionSerializer, UpdateSerializer, DiscordGuildSerializer
 
 class UserViewSet(ModelViewSet):
 	serializer_class = UserSerializer
@@ -30,3 +31,21 @@ class DiscordGuildViewSet(ModelViewSet):
 	serializer_class = DiscordGuildSerializer
 	queryset = DiscordGuild.objects.all()
 	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+def profile(request, username):
+	try:
+		trainer = Trainer.objects.get(username__iexact=username)
+		updates = Update.objects.filter(trainer=trainer)
+		xp = 0
+		for i in updates:
+			if i.xp > xp:
+				xp = i.xp
+		#user = Trainer.owner
+	except Trainer.DoesNotExist:
+		raise Http404("User not found")
+	context = {
+		'trainer' : trainer,
+		'update_list' : updates,
+		'xp': xp,
+	}
+	return render(request, 'profile.html', context)
