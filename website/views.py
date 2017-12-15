c2bbc92b0759b3265910ba8d5af1b9c68250869d
Jay Turner <jaynicholasturner@gmail.com>
2017-12-15 21:35:44 +0000

    Filter community list via team

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from website.models import Discord, FacebookGroup, WhatsApp, MessengerGroup
+from trainer.models import ExtendedProfile, Faction
 from itertools import chain
 
 def index(request):
@@ -12,6 +13,8 @@ def communities(request):
 	messengergroup_list = MessengerGroup.objects.all()
 	whatsapp_list = WhatsApp.objects.all()
 	community_list = list(chain(discord_list, facebookgroup_list, whatsapp_list, messengergroup_list))
+	if request.user.is_authenticated():
+		community_list = list(filter(lambda x: x.team in (ExtendedProfile.objects.get(user=request.user).prefered_profile.faction, Faction.objects.get(pk=0)), community_list))
 	community_list.sort(key=lambda x: x.name)
 	context = {
 		'community_list' : community_list,
