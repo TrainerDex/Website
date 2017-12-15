eafee26d1370104d81abb9260f01112316d9c29d
Jay Turner <jaynicholasturner@gmail.com>
2017-12-15 19:28:36 +0000

    Temporarily disabled new registrations

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from website.models import Discord, FacebookGroup, WhatsApp, MessengerGroup
 from itertools import chain
 
@@ -17,3 +17,6 @@ def communities(request):
 		'community_list' : community_list,
 	}
 	return render(request, 'communities.html', context)
+
+def discord(request):
+	return redirect('https://discord.gg/pFhMS3s')
