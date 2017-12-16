b12106b7d81ee3e9d114ec88e08c90419c82a60f
Jay Turner <jaynicholasturner@gmail.com>
2017-12-16 18:06:33 +0000

    Simple XP dialog

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from rest_framework.routers import SimpleRouter
 from trainer.views import *
 
@@ -9,3 +10,4 @@ router.register("update", UpdateViewSet)
 router.register("guilds", DiscordGuildViewSet)
 
 urlpatterns = router.urls
+urlpatterns.append(url(r'^tools/update_dialog/$', update_dialog, name='update_dialog'))
