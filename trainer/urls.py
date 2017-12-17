2aa7db09a7ea8e5a4cc279cedb790b4c4cf6e31e
Jay Turner <jaynicholasturner@gmail.com>
2017-12-17 17:59:45 +0000

    TrainerListView

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from trainer.views import TrainerListView, TrainerDetailView, UpdateListView, UpdateDetailView, TrainerOwnerRedirect
+from trainer.views import TrainerListView, TrainerDetailView, UpdateListView, UpdateDetailView, TrainerOwnerRedirect, QuickUpdateDialogView
 
 
 urlpatterns = [
-    url(r'^trainers/', TrainerListView),
-    url(r'^trainers/<int:id>', TrainerDetailView),
-    url(r'^trainers/<int:trainer>/updates/', UpdateListView),
-    url(r'^trainers/<int:trainer>/updates/<int:uuid>/', UpdateDetailView),
-    url(r'^trainers/<int:id>/owner', TrainerOwnerRedirect),
+    url(r'^$', TrainerListView.as_view()),
+    url(r'^<int:id>/$', TrainerDetailView.as_view()),
+    url(r'^<int:trainer>/updates/$', UpdateListView.as_view()),
+    url(r'^<int:trainer>/updates/<int:uuid>/$', UpdateDetailView.as_view()),
+    url(r'^<int:id>/owner/$', TrainerOwnerRedirect.as_view()),
+    url(r'^tools/update_dialog/$', QuickUpdateDialogView, name='update_dialog'),
 ]
