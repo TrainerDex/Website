871110f561ef24f2bd7b1ee960f4b523de7d788e
Jay Turner <jaynicholasturner@gmail.com>
2017-12-18 14:49:07 +0000

    LeaderboardAPIView

--- ekpogo/urls.py
+++ ekpogo/urls.py
@@ -5,10 +5,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 from ajax_select import urls as ajex_select_urls
 from website.views import *
-from trainer.views import TrainerProfileView, LeaderboardView
+from trainer.views import TrainerProfileView, LeaderboardView, LeaderboardAPIView
+from trainer.urls import TrainerURLS
 
 api_v1_patterns = [
-    url(r'^trainers/', include('trainer.urls', namespace="trainer_profiles")),
+    url(r'^trainers/', include(TrainerURLS, namespace="trainer_profiles")),
+    url(r'^leaderboard/$', LeaderboardAPIView.as_view()),
 #    url(r'^gyms/', include('raids.urls', namespace="raid_enrollment")),
 ]
 
--- trainer/urls.py
+++ trainer/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
-from trainer.views import TrainerListView, TrainerDetailView, UpdateListView, UpdateDetailView, TrainerOwnerRedirect, QuickUpdateDialogView
+from trainer.views import TrainerListView, TrainerDetailView, UpdateListView, UpdateDetailViewLatest, UpdateDetailView, QuickUpdateDialogView
 from trainer.errors import ThrowMalformedPKError, ThrowMalformedUUIDError
 
-urlpatterns = [
-    url(r'^$', TrainerListView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', TrainerDetailView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/updates/$', UpdateListView.as_view()),
-    url(r'^(?P<pk>[0-9]+)/updates/(?P<uuid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$', UpdateDetailView.as_view()),
-    url(r'^(?P<pk>.+)/updates/(?P<uuid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$', ThrowMalformedPKError),
-    #url(r'^(?P<pk>[0-9]+)/updates/(.+)', ThrowMalformedUUIDError),
-    url(r'^(?P<pk>[0-9]+)/owner/$', TrainerOwnerRedirect.as_view()),
-    url(r'^tools/update_dialog/$', QuickUpdateDialogView, name='update_dialog'),
-]
+class TrainerURLS:
+    
+    urlpatterns = [
+        url(r'^$', TrainerListView.as_view()),
+        url(r'^(?P<pk>[0-9]+)/$', TrainerDetailView.as_view()),
+        url(r'^(?P<pk>[0-9]+)/updates/$', UpdateListView.as_view()),
+        url(r'^(?P<pk>[0-9]+)/updates/latest/$', UpdateDetailViewLatest.as_view()),
+        url(r'^(?P<pk>[0-9]+)/updates/(?P<uuid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$', UpdateDetailView.as_view()),
+        url(r'^(?P<pk>.+)/updates/(?P<uuid>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$', ThrowMalformedPKError),
+        #url(r'^(?P<pk>[0-9]+)/updates/(.+)', ThrowMalformedUUIDError),
+        url(r'^tools/update_dialog/$', QuickUpdateDialogView, name='update_dialog'),
+    ]
