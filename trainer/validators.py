0d08e133ed8d5249505d98d66d530ba0d6fa36e9
Jay Turner <jaynicholasturner@gmail.com>
2018-01-02 23:06:45 +0000

    Add Translation files

--- trainer/validators.py
+++ trainer/validators.py
@@ -3,5 +3,5 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 def validate_startdate(value):
-	if value < date(2016, 7, 6):
+	if value < date(2016, 7, 5):
 		raise ValidationError(_("The date you entered was before launch date of 6th July 2016"), params={'value': value},)
