4ec9c4b38254962db02f090cca0e0a864883674a
Jay Turner <jaynicholasturner@gmail.com>
2018-01-06 19:59:15 +0000

    Improved Admin interface

--- trainer/admin.py
+++ trainer/admin.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from ajax_select.admin import AjaxSelectAdmin
 from ajax_select import make_ajax_form
 from trainer.models import *
@@ -25,10 +26,32 @@ class UpdateAdmin(AjaxSelectAdmin):
 class TrainerAdmin(AjaxSelectAdmin):
 	
 	form = make_ajax_form(Trainer, {
-		'username' : 'user'
+		'owner' : 'user',
+		'leaderboard_country' : 'countries',
+		'leaderboard_region': 'regions',
+		'leaderboard_subregion' : 'subregions',
+		'leaderboard_city' : 'cities',
 	})
 	list_display = ('username', 'faction', 'currently_cheats', 'statistics')
 	list_filter = ('faction', 'has_cheated', 'currently_cheats', 'statistics')
 	search_fields = ('username', 'owner__username', 'owner__first_name')
 	ordering = ('username',)
 	date_hierarchy = 'start_date'
+	fieldsets = (
+		(None, {
+			'fields': ('owner', 'username', 'faction', 'start_date', 'active', 'statistics', 'daily_goal', 'total_goal')
+		}),
+		(_('Reports'), {
+			'fields': ('has_cheated', 'last_cheated', 'currently_cheats')
+		}),
+		(_('Medals'), {
+			'fields': ('go_fest_2017', 'outbreak_2017', 'safari_zone_2017_oberhausen', 'safari_zone_2017_paris', 'safari_zone_2017_barcelona', 'safari_zone_2017_copenhagen', 'safari_zone_2017_prague', 'safari_zone_2017_stockholm', 'safari_zone_2017_amstelveen')
+		}),
+		(_('Leaderboard'), {
+			'fields': ('leaderboard_country', 'leaderboard_region', 'leaderboard_subregion', 'leaderboard_city')
+		}),
+		#(_('Play Zones'), {
+		#	'fields': ('play_zones_country', 'play_zones_region', 'play_zones_subregion', 'play_zones_city')
+		#}),
+	)
+	
