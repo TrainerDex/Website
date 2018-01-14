635fcc16bc2900352b0e239aa8a75981a76e358b
JayTurner <jayturnr@trainerdex.co.uk>
2018-01-14 21:55:09 +0000

    Security bugs

--- ekpogo/settings.py
+++ ekpogo/settings.py
@@ -153,8 +153,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    )
+        'rest_framework.authentication.TokenAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
 }
 
 # Django AllAuth
