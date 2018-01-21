4e51388d64832195530ffec2256a65c090bed9f8
Jay Turner <jaynicholasturner@gmail.com>
2018-01-21 15:22:52 +0000

    Validation for Trainer username

--- trainer/validators.py
+++ trainer/validators.py
@@ -4,5 +4,10 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 def validate_startdate(value):
-	if value < date(2016, 7, 5):
-		raise ValidationError(_("The date you entered was before launch date of 6th July 2016"), params={'value': value},)
+	LAUNCH_DATE = date(2016, 7, 6)
+	if value < LAUNCH_DATE:
+		raise ValidationError(_("The date you entered was before launch date of {launch_date}").format(launch_date=LAUNCH_DATE), params={'value': value},)
+
+def validate_pokemon_go_username(value):
+	if not (4 <= len(value) <= 15):
+		raise ValidationError(_("The username you entered is not a valid Pokemon Go username. If you believe this to be incorrect, please contact support at {support_email}").format(support_email='support@trainerdex.co.uk'), params={'value': value},)
