6ff70bef1731677501e5ba0d99ac734a3ede24a0
Jay Turner <jaynicholasturner@gmail.com>
2018-01-21 12:07:43 +0000

    Changed Email Subject for verification

--- trainer/views.py
+++ trainer/views.py
@@ -608,7 +608,7 @@ def RegistrationView(request):
 			update.meta_source = 'ts_registration'
 			update_form.save()
 			messages.success(request, _("Thanks for registering. You are now logged in."))
-			email_subject = _("TrainerDex Verification").format(trainer=trainer.username)
+			email_subject = _("[TrainerDex] Verification needed for {trainer}").format(trainer=trainer.username)
 			email_message = _("Welcome {trainer}, please respond to this email with screenshots of the top and bottom of your Pokemon Go profile for verification. Once verified, you will appear in the leaderboards.").format(trainer=trainer.username)
 			mail = EmailMessage(email_subject, email_message, to=(user.email,))
 			mail.send()
