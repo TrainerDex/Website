a096945d8f82248c4a7cc866482e0f7802238c8d
Jay Turner <jaynicholasturner@gmail.com>
2018-07-09 12:44:16 +0100

    Update Pokemon Go Username Validator

--- trainer/validators.py
+++ trainer/validators.py
@@ -2,14 +2,16 @@
 from datetime import date, timedelta
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+import re
 
 def StartDateValidator(value):
 	LAUNCH_DATE = date(2016, 7, 6)
 	if not date.today()+timedelta(days=1) >= value >= LAUNCH_DATE:
-		raise ValidationError(_("The date you entered was before launch date of {launch_date} or in the future.").format(launch_date=LAUNCH_DATE), params={'value': value},)
+		raise ValidationError(_("The date you entered was before launch date of {launch_date} or in the future.").format(launch_date=LAUNCH_DATE), params={'value': value})
 
 def PokemonGoUsernameValidator(value):
-	if not (4 <= len(value) <= 15):
-		raise ValidationError(_("The username you entered is not a valid Pokemon Go username."), params={'value': value},)
+	match = re.match('^[A-Za-z0-9]{3,15}$', value)
+	if not match:
+		raise ValidationError(_("The username you entered is not a valid Pokemon Go username."), params={'value': value})
 
 username_validator = [PokemonGoUsernameValidator]
