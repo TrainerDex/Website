aa24823c7dd4264a0650b1e9d033e7e57d0fb39b
Jay Turner <github@jayturnr.me>
2018-08-17 13:32:16 +0100

    Fix Indentation

--- trainer/sitemaps.py
+++ trainer/sitemaps.py
@@ -5,7 +5,7 @@ from cities.models import Continent, Country, Region
 
 class BaseSitemap(Sitemap):
 	changefreq = "daily"
-
+	
 	def items(self):
 		return [
 			('account_settings', 0.9),
@@ -13,63 +13,63 @@ class BaseSitemap(Sitemap):
 			('help:faq', 0.9),
 			('trainerdex_web:update_stats', 0.9)
 		]
-
+	
 	def priority(self, obj):
 		return obj[1]
-
+	
 	def location(self, obj):
 		return reverse(obj[0])
 
 class TrainerSitemap(Sitemap):
 	changefreq = "weekly"
-
+	
 	def items(self):
 		return Trainer.objects.exclude(statistics=False).exclude(verified=False).exclude(currently_cheats = True).filter(update__isnull=False).distinct()
-
+	
 	def lastmod(self, obj):
 		return obj.last_modified
-
+	
 	def priority(self, obj):
 		return min((min(obj.update_set.order_by('-xp')[0].xp/20000000, 1.0)*(5/11))+0.5, 0.9)
 
 class LeaderboardContinentSitemap(Sitemap):
 	changefreq = "daily"
 	priority = 1.0
-
+	
 	def items(self):
 		return Continent.objects.exclude(code='AN')
-
+	
 	def location(self, obj):
 		return reverse('trainerdex_web:leaderboard', kwargs={'continent':obj.code})
 
 class LeaderboardCountrySitemap(Sitemap):
 	changefreq = "daily"
-
+	
 	def items(self):
 		return Country.objects.filter(leaderboard_trainers_country__isnull = False).distinct()
-
+	
 	def priority(self, obj):
 		count = obj.leaderboard_trainers_country.count()
 		if count:
 			return 0.92 + min(count, 20)/400
 		else:
 			return 0.02
-
+	
 	def location(self, obj):
 		return reverse('trainerdex_web:leaderboard', kwargs={'country':obj.code})
 
 class LeaderboardRegionSitemap(Sitemap):
 	changefreq = "daily"
-
+	
 	def items(self):
 		return Region.objects.filter(leaderboard_trainers_region__isnull = False).distinct()
-
+	
 	def priority(self, obj):
 		count = obj.leaderboard_trainers_region.count()
 		if count:
 			return 0.92 + min(count, 20)/400
 		else:
 			return 0.02
-
+	
 	def location(self, obj):
 		return reverse('trainerdex_web:leaderboard', kwargs={'country':obj.country.code, 'region': obj.code})
