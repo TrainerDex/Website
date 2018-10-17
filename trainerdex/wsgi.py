55ac35c7344c5ff53bf333df455e8a3552d1915a
Jay Turner <jaynicholasturner@gmail.com>
2018-10-17 09:58:29 +0100

    Cleanup Part One: Change references of ekpogo to trainerdex

--- trainerdex/wsgi.py
+++ trainerdex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ekpogo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trainerdex.settings")
 
 application = get_wsgi_application()
