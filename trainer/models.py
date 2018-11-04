TrainerDex/Website

commit a138a25e326a7df039ef2fae78765b4d7fb7b46a

Segregation: Segregate API related functions into their own sub-app

trainer/models.py
@@ -13,7 +13,8 @@ from datetime import date, datetime, timedelta, timezone, time
 from decimal import Decimal
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from django.contrib.postgres import fields as postgres_fields
 from django.core.exceptions import ValidationError, ObjectDoesNotExist, PermissionDenied
 from django.db import models
@@ -35,7 +36,7 @@ def VerificationUpdateImagePath(instance, filename):
 
 class Trainer(models.Model):
 	
-	owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='trainer', verbose_name=_("User"))
+	owner = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='trainer', verbose_name=_("User"))
 	username = postgres_fields.CICharField(max_length=15, unique=True, validators=[PokemonGoUsernameValidator], db_index=True, verbose_name=pgettext_lazy("onboard_enter_name_hint", "Nickname"), help_text=_("Your Trainer Nickname exactly as is in game. You are free to change capitalisation but removal or addition of digits may prevent other Trainers with similar usernames from using this service and is against the Terms of Service."))
 	start_date = models.DateField(null=True, blank=True, validators=[MinValueValidator(date(2016, 7, 5))], verbose_name=pgettext_lazy("profile_start_date", "Start Date"), help_text=_("The date you created your Pokémon Go account."))
 	faction = models.ForeignKey('Faction', on_delete=models.SET_DEFAULT, default=0, verbose_name=_("Team"), help_text=_("Mystic = Blue, Instinct = Yellow, Valor = Red.") )
@@ -1117,7 +1118,7 @@ class Update(models.Model):
 
 class TrainerReport(models.Model):
 	trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, verbose_name=_("Trainer"))
-	reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("Reported by"))
+	reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=_("Reported by"))
 	report = models.TextField(verbose_name=_("Report"))
 	
 	class Meta:

trainer/views.py
@@ -3,342 +3,24 @@ import logging
 logger = logging.getLogger('django.trainerdex')
 import requests
 
-from allauth.socialaccount.models import SocialAccount
 from cities.models import Continent, Country, Region
-from datetime import datetime, timedelta, date
+from datetime import datetime, date
 from django import forms
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError, ObjectDoesNotExist, PermissionDenied
-from django.core.mail import mail_admins, EmailMessage
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.db.models import Max, Q, Sum, F, Window
 from django.db.models.functions import Rank
-from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, QueryDict, HttpResponseBadRequest, Http404, HttpResponse
-from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect, reverse
-from django.utils import timezone
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.utils.translation import gettext_lazy as _
-from django.utils.translation import gettext_noop, pgettext_lazy, get_language_from_request
-from django.urls import resolve
+from django.utils.translation import get_language_from_request
 from math import ceil
-from pytz import utc
-from rest_framework import authentication, permissions, status
-from rest_framework.decorators import detail_route
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from trainer.forms import UpdateForm, RegistrationFormTrainer, RegistrationFormUpdate
 from trainer.models import Trainer, Update, Faction
-from trainer.serializers import UserSerializer, BriefTrainerSerializer, DetailedTrainerSerializer, BriefUpdateSerializer, DetailedUpdateSerializer, LeaderboardSerializer, SocialAllAuthSerializer
-from trainer.shortcuts import strtoboolornone, cleanleaderboardqueryset, level_parser, UPDATE_FIELDS_BADGES, UPDATE_FIELDS_TYPES, UPDATE_SORTABLE_FIELDS, BADGES, chunks
-
-
-def _leaderboard_queryset_filter(queryset):
-	return queryset.exclude(update__isnull=True).exclude(statistics=False).exclude(verified=False).exclude(last_cheated__lt=date(2018,9,1)-timedelta(weeks=26)).exclude(last_cheated__gt=date(2018,9,1)).exclude(last_cheated__gt=date.today()-timedelta(weeks=26)).select_related('faction')
-
-# RESTful API Views
-
-def recent(value):
-	if timezone.now()-timedelta(hours=1) <= value <= timezone.now():
-		return True
-	return False
-
-class UserViewSet(ModelViewSet):
-	serializer_class = UserSerializer
-	queryset = User.objects.all()
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-class TrainerListJSONView(APIView):
-	"""
-	get:
-	Accepts paramaters for Team (t) and Username (q)
-	
-	post:
-	Register a Trainer, needs the Primary Key of the Owner, the User object which owns the Trainer
-	"""
-	
-	authentication_classes = (authentication.TokenAuthentication,)
-	
-	def get(self, request):
-		queryset = Trainer.objects.exclude(owner__is_active=False)
-		if request.GET.get('q') or request.GET.get('t'):
-			if request.GET.get('q'):
-				queryset = queryset.filter(username__iexact=request.GET.get('q'))
-			if request.GET.get('t'):
-				queryset = queryset.filter(faction=request.GET.get('t'))
-		
-		if request.GET.get('detail') == '1':
-			serializer = DetailedTrainerSerializer(queryset, many=True)
-			return Response(serializer.data)
-		else:
-			serializer = BriefTrainerSerializer(queryset, many=True)
-			return Response(serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
-	
-	def post(self, request):
-		"""
-		This used to work as a simple post, but since the beginning of transitioning to API v2 it would have always given Validation Errors if left the same.
-		Now it has a 60 minute open slot to work after the auth.User (owner) instance is created. After which, a PATCH request must be given. This is due to the nature of a Trainer being created automatically for all new auth.User
-		"""
-		
-		trainer = Trainer.objects.get(owner__pk=request.data['owner'])
-		if not recent(trainer.owner.date_joined):
-			return Response({"_error": "profile already exists, please use patch on trainer uri instead or check the owner pk is correct", "_profile_id": trainer.pk}, status=status.HTTP_400_BAD_REQUEST)
-		serializer = DetailedTrainerSerializer(trainer, data=request.data, partial=True)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-
-class TrainerDetailJSONView(APIView):
-	"""
-	get:
-	Trainer detail
-	
-	patch:
-	Update a trainer
-	
-	delete:
-	Archives a trainer
-	"""
-	
-	authentication_classes = (authentication.TokenAuthentication,)
-	
-	def get_object(self, pk):
-		return get_object_or_404(Trainer, pk=pk)
-	
-	def get(self, request, pk):
-		trainer = self.get_object(pk)
-		if trainer.active is True:
-			if request.GET.get('detail') == 'low':
-				serializer = BriefTrainerSerializer(trainer)
-			else:
-				serializer = DetailedTrainerSerializer(trainer)
-			if trainer.statistics is True or (trainer.statistics is False and request.GET.get('statistics') == 'force'):
-				return Response(serializer.data)
-			elif trainer.statistics is False:
-				return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
-		elif trainer.active is False:
-			response = {
-				'code': 1,
-				'reason': 'Profile deactivated',
-				'profile': {
-					'id': trainer.pk,
-					'faction': trainer.faction.id,
-				},
-			}
-			return Response(response, status=status.HTTP_423_LOCKED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def patch(self, request, pk):
-		trainer = self.get_object(pk)
-		serializer = DetailedTrainerSerializer(trainer, data=request.data, partial=True)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_200_OK)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def delete(self, request, pk):
-		trainer = self.get_object(pk)
-		if trainer.active:
-			trainer.active = False
-			trainer.save()
-			response = {
-				'code': 1,
-				'reason': 'Profile deactivated',
-				'profile': {
-					'id': trainer.pk,
-					'faction': trainer.faction.id,
-				},
-			}
-			return Response(response, status=status.HTTP_204_NO_CONTENT)
-		return Response(status=status.HTTP_400_BAD_REQUEST)
-	
-
-class UpdateListJSONView(APIView):
-	"""
-	get:
-	Takes Trainer ID as part of URL, optional param: detail, shows all detail, otherwise, returns a list of objects with fields 'time_updated' (datetime), 'xp'(int) and 'fields_updated' (list)
-	
-	post:
-	Create a update
-	"""
-	
-	authentication_classes = (authentication.TokenAuthentication,)
-	
-	def get(self, request, pk):
-		updates = Update.objects.filter(trainer=pk)
-		serializer = BriefUpdateSerializer(updates, many=True) if request.GET.get('detail') != "1" else DetailedUpdateSerializer(updates, many=True)
-		return Response(serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
-	
-	def post(self, request, pk):
-		serializer = DetailedUpdateSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save(trainer=get_object_or_404(Trainer, pk=pk))
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def patch(self, request, pk):
-		return self.post(self, request, pk)
-	
-
-class LatestUpdateJSONView(APIView):
-	"""
-	get:
-	Gets detailed view of the latest update
-	
-	patch:
-	Allows editting of update within first half hour of creation, after that time, all updates are denied. Trainer, UUID and PK are locked.
-	"""
-	
-	authentication_classes = (authentication.TokenAuthentication,)
-	
-	def get(self, request, pk):
-		update = Update.objects.filter(trainer=pk).latest('update_time')
-		serializer = DetailedUpdateSerializer(update)
-		return Response(serializer.data)
-	
-	def patch(self, request, pk):
-		update = Update.objects.filter(trainer=pk).latest('update_time')
-		if update.meta_time_created > datetime.now(utc)-timedelta(minutes=32):
-			serializer = DetailedUpdateSerializer(update, data=request.data)
-			if serializer.is_valid():
-				serializer.clean()
-				serializer.save(trainer=update.trainer,uuid=update.uuid,pk=update.pk)
-				return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-
-class UpdateDetailJSONView(APIView):
-	"""
-	get:
-	Gets detailed view
-	
-	patch:
-	Allows editting of update within first half hour of creation, after that time, all updates are denied. Trainer, UUID and PK are locked
-	
-	delete:
-	Delete an update, works within first 48 hours of creation, otherwise, an email request for deletion is sent to admin
-	"""
-	
-	authentication_classes = (authentication.TokenAuthentication,)
-	
-	def get(self, request, uuid, pk):
-		update = get_object_or_404(Update, trainer=pk, uuid=uuid)
-		serializer = DetailedUpdateSerializer(update)
-		if update.trainer.id != int(pk):
-			return Response(serializer.errors, status=400)
-		return Response(serializer.data)
-	
-	def patch(self, request, uuid, pk):
-		update = get_object_or_404(Update, trainer=pk, uuid=uuid)
-		if update.meta_time_created > datetime.now(utc)-timedelta(minutes=32):
-			serializer = DetailedUpdateSerializer(update, data=request.data)
-			if serializer.is_valid():
-				serializer.clean()
-				serializer.save(trainer=update.trainer,uuid=update.uuid,pk=update.pk)
-				return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
-	def delete(self, request, uuid, pk):
-		update = get_object_or_404(Update, trainer=pk, uuid=uuid)
-		if update.meta_time_created > datetime.now(utc)-timedelta(days=2):
-			update.delete()
-			return Response(status=status.HTTP_204_NO_CONTENT)
-		else:
-			SUBJECT = 'Late Update Deletion Request'
-			MESSAGE = 'There has been a request to delete {update} by {requester} at {ip}'.format(update=request.build_absolute_uri(), requester=request.user, ip=request.get_host())
-			mail_admins(SUBJECT, MESSAGE)
-			return Response({'request_made': True, 'subject': SUBJECT, 'message': MESSAGE}, status=status.HTTP_202_ACCEPTED)
-		return Response(status=status.HTTP_400_BAD_REQUEST)
-	
-
-class LeaderboardJSONView(APIView):
-	"""
-	Limited to 5000
-	"""
-	
-	def get(self, request):
-		query = _leaderboard_queryset_filter(Trainer.objects)
-		if request.GET.get('users'):
-			query = Trainer.objects.filter(id__in=request.GET.get('users').split(','))
-		leaderboard = query.prefetch_related('faction').prefetch_related('update_set').prefetch_related('owner').annotate(Max('update__total_xp'), Max('update__update_time')).exclude(update__total_xp__max__isnull=True).annotate(rank=Window(expression=Rank(), order_by=F(f'update__total_xp__max').desc())).order_by('rank')
-		serializer = LeaderboardSerializer(leaderboard[:5000], many=True)
-		return Response(serializer.data)
-	
-
-class SocialLookupJSONView(APIView):
-	"""
-	get:
-		kwargs:
-			provider (requiered) - platform, options are 'facebook', 'twitter', 'discord', 'google', 'patreon'
-		
-			uid - Social ID, supports a comma seperated list. Could be useful for passing a list of users in a server to retrieve a list of UserIDs, which could then be passed to api/v1/leaderboard/
-			user - TrainerDex User ID, supports a comma seperated list
-			trainer - TrainerDex Trainer ID
-	
-	patch:
-	Register a SocialAccount. Patch if exists, post if not.
-	"""
-	
-	def get(self, request):
-		query = SocialAccount.objects.filter(provider=request.GET.get('provider'))
-		if request.GET.get('uid'):
-			query = query.filter(uid__in=request.GET.get('uid').split(','))
-		elif request.GET.get('user'):
-			query = query.filter(user__in=request.GET.get('user').split(','))
-		elif request.GET.get('trainer'):
-			query = query.filter(user=get_object_or_404(Trainer, pk=request.GET.get('trainer')).owner)
-		else:
-			return Response(status=status.HTTP_400_BAD_REQUEST)
-		serializer = SocialAllAuthSerializer(query, many=True)
-		return Response(serializer.data)
-		
-	def put(self, request):
-		try:
-			query = SocialAccount.objects.get(provider=request.data['provider'], uid=request.data['uid'])
-		except:
-			serializer = SocialAllAuthSerializer(data=request.data)
-		else:
-			serializer = SocialAllAuthSerializer(query, data=request.data, partial=True)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class DiscordLeaderboardAPIView(APIView):
-	def get(self, request, guild):
-		discord_base_url = 'https://discordapp.com/api'
-		headers = {}
-		output = {'generated':datetime.utcnow()}
-		
-		if request.GET.get('DiscordAuthorization'):
-			headers['Authorization'] = request.GET.get('DiscordAuthorization')
-		else:
-			headers['Authorization'] = 'Bot Mzc3NTU5OTAyNTEzNzkwOTc3.DYhWdw.tBGXI2g8RqH3EbDDdypSaQLXYLU'
-		
-		_guild = requests.get('{base_url}/guilds/{param}'.format(base_url=discord_base_url, param=guild), headers=headers)
-		if not status.is_success(_guild.status_code):
-			return Response({'error': '003 - Bad Guild ID', 'cause': "Most likely, the bot parameter provided doesn't have access to that guild", 'solution': "If I have to lay this out to you, you shouldn't be here"}, status=status.HTTP_400_BAD_REQUEST)
-		_guild = _guild.json()
-		
-		output['title'] = '{guild_name} Leaderboard'.format(guild_name=_guild['name'])
-		opt_out_role_id = [x['id'] for x in _guild['roles'] if x['name'] in ('NoLB,')]
-		
-		members = requests.get('{base_url}/guilds/{param}/members'.format(base_url=discord_base_url, param=guild), headers=headers, params={'limit':1000})
-		if not status.is_success(members.status_code):
-			return Response({'error': '000 - Unknown', 'cause': 'unknown', 'solution':'forward this output to apisupport@trainerdex.co.uk', 'Discord API Responce': members.content}, status=members.status_code)
-		members = [x['user']['id'] for x in members.json() if not any([i in x['roles'] for i in opt_out_role_id])]
-		trainers = _leaderboard_queryset_filter(Trainer.objects.filter(owner__socialaccount__provider='discord', owner__socialaccount__uid__in=members))
-		
-		leaderboard = trainers.prefetch_related('faction').prefetch_related('update_set').prefetch_related('owner').annotate(Max('update__total_xp'), Max('update__update_time')).exclude(update__total_xp__max__isnull=True).annotate(rank=Window(expression=Rank(), order_by=F(f'update__total_xp__max').desc())).order_by('rank')
-		serializer = LeaderboardSerializer(leaderboard, many=True)
-		output['leaderboard'] = serializer.data
-		return Response(output)
-	
-# Web-based views
+from trainer.shortcuts import strtoboolornone, filter_leaderboard_qs, level_parser, UPDATE_FIELDS_BADGES, UPDATE_FIELDS_TYPES, UPDATE_SORTABLE_FIELDS, BADGES, chunks
 
 def _check_if_trainer_valid(trainer):
 	if settings.DEBUG:
@@ -504,7 +186,7 @@ def LeaderboardHTMLView(request, continent=None, country=None, region=None):
 		context['title'] = None
 		QuerySet = Trainer.objects
 	
-	QuerySet = _leaderboard_queryset_filter(QuerySet)
+	QuerySet = filter_leaderboard_qs(QuerySet)
 	
 	SORTABLE_FIELDS = ['update__'+x for x in UPDATE_SORTABLE_FIELDS]
 	fields_to_calculate_max = {'total_xp', 'badge_capture_total', 'badge_travel_km', 'badge_evolved_total', 'badge_hatched_total', 'badge_pokestops_visited', 'badge_raid_battle_won', 'badge_legendary_battle_won',

commit 540600869c269c2850270fad6e478e0f9424cb48

Hotfix: Fix Broken references
This fixes broken references caused by a138a25e326a7df039ef2fae78765b4d7fb7b46a

trainer/models.py
@@ -242,7 +242,7 @@ class Trainer(models.Model):
                 raise ValidationError({'thesilphroad_username': _("The team of this Silph Card does not match that of your profile.")})
     
     def get_absolute_url(self):
-        return reverse('trainerdex_web:profile_username', kwargs={'username':self.username})
+        return reverse('trainerdex:profile_username', kwargs={'username':self.username})
     
     class Meta:
         ordering = ['username']

trainer/sitemaps.py
@@ -9,9 +9,9 @@ class BaseSitemap(Sitemap):
     def items(self):
         return [
             ('account_settings', 0.9),
-            ('trainerdex_web:leaderboard', 1),
+            ('trainerdex:leaderboard', 1),
             ('help:faq', 0.9),
-            ('trainerdex_web:update_stats', 0.9)
+            ('trainerdex:update_stats', 0.9)
         ]
     
     def priority(self, obj):
@@ -40,7 +40,7 @@ class LeaderboardContinentSitemap(Sitemap):
         return Continent.objects.exclude(code='AN')
     
     def location(self, obj):
-        return reverse('trainerdex_web:leaderboard', kwargs={'continent':obj.code})
+        return reverse('trainerdex:leaderboard', kwargs={'continent':obj.code})
 
 class LeaderboardCountrySitemap(Sitemap):
     changefreq = "daily"
@@ -56,7 +56,7 @@ class LeaderboardCountrySitemap(Sitemap):
             return 0.02
     
     def location(self, obj):
-        return reverse('trainerdex_web:leaderboard', kwargs={'country':obj.code})
+        return reverse('trainerdex:leaderboard', kwargs={'country':obj.code})
 
 class LeaderboardRegionSitemap(Sitemap):
     changefreq = "daily"
@@ -72,4 +72,4 @@ class LeaderboardRegionSitemap(Sitemap):
             return 0.02
     
     def location(self, obj):
-        return reverse('trainerdex_web:leaderboard', kwargs={'country':obj.country.code, 'region': obj.code})
+        return reverse('trainerdex:leaderboard', kwargs={'country':obj.country.code, 'region': obj.code})

trainer/templates/create_update.html
@@ -139,7 +139,7 @@
       
 {% block extra_body %}
 <script type="text/javascript">
-  const url = '{% url 'trainerdex_rest.1:latest_update' user.trainer.pk %}'
+  const url = '{% url 'trainerdex.api.1:latest_update' user.trainer.pk %}'
   
   function createNode(element) {
     return document.createElement(element); // Create the type of element you pass in the parameters

trainer/templates/leaderboard.html
@@ -73,7 +73,7 @@ function setParam(uri, key, val) {
     {% endif %}
       <tr>
         <td>{{trainer.position|ordinal}}</td>
-        <td class="mdl-data-table__cell--non-numeric" style="width: auto; white-space: nowrap;" onclick="location.href = '{% url 'trainerdex_web:profile' %}?id={{trainer.trainer.pk}}'"><div class="trainer-nickname">{{trainer.trainer.username}} {{trainer.trainer.flag_emoji|default_if_none:''}}</div> <div class="tl">{{trainer.level}}</div></td>
+        <td class="mdl-data-table__cell--non-numeric" style="width: auto; white-space: nowrap;" onclick="location.href = '{% url 'trainerdex:profile' %}?id={{trainer.trainer.pk}}'"><div class="trainer-nickname">{{trainer.trainer.username}} {{trainer.trainer.flag_emoji|default_if_none:''}}</div> <div class="tl">{{trainer.level}}</div></td>
         <td class="mdl-data-table__cell--non-numeric" style="padding-top: 0px; padding-bottom: 0px; width: 32px;" ><img width="32px" src="{% static trainer.trainer.faction.vector_image %}" onerror="this.src='{% static trainer.trainer.faction.image %}'" alt="{{trainer.trainer.faction.name}}"/></td>
         {% for column in trainer.columns %}
         <td>{{column.value|default_if_none:"---"|intcomma}}</td>

trainer/views.py
@@ -51,7 +51,7 @@ def TrainerRedirectorView(request, username=None, id=None):
     else:
         trainer = request.user.trainer
     
-    return HttpResponsePermanentRedirect(reverse('trainerdex_web:profile_username', kwargs={'username':trainer.username}))
+    return HttpResponsePermanentRedirect(reverse('trainerdex:profile_username', kwargs={'username':trainer.username}))
 
 def TrainerProfileHTMLView(request, username):
     if request.user.is_authenticated and not _check_if_self_valid(request):
@@ -136,7 +136,7 @@ def CreateUpdateHTMLView(request):
         if form.is_valid():
             update = form.save()
             messages.success(request, 'Statistics updated')
-            return HttpResponseRedirect(reverse('trainerdex_web:profile_username', kwargs={'username':request.user.trainer.username}))
+            return HttpResponseRedirect(reverse('trainerdex:profile_username', kwargs={'username':request.user.trainer.username}))
     
     context = {
         'form': form
@@ -267,7 +267,7 @@ def SetUpProfileViewStep2(request):
     if request.user.is_authenticated and _check_if_self_valid(request):
         if len(request.user.trainer.update_set.all()) == 0:
             return HttpResponseRedirect(reverse('profile_first_post'))
-        return HttpResponseRedirect(reverse('trainerdex_web:profile'))
+        return HttpResponseRedirect(reverse('trainerdex:profile'))
     
     form = RegistrationFormTrainer(instance=request.user.trainer)
     form.fields['verification'].required = True
@@ -286,7 +286,7 @@ def SetUpProfileViewStep2(request):
 @login_required
 def SetUpProfileViewStep3(request):
     if request.user.is_authenticated and _check_if_self_valid(request) and len(request.user.trainer.update_set.all()) > 0:
-        return HttpResponseRedirect(reverse('trainerdex_web:update_stats'))
+        return HttpResponseRedirect(reverse('trainerdex:update_stats'))
     
     form = RegistrationFormUpdate(initial={'trainer':request.user.trainer})
     form.fields['screenshot'].required = True
@@ -302,7 +302,7 @@ def SetUpProfileViewStep3(request):
         if form.is_valid():
             form.save()
             messages.success(request, _("Stats updated. Screenshot included."))
-            return HttpResponseRedirect(reverse('trainerdex_web:profile_username', kwargs={'username':request.user.trainer.username}))
+            return HttpResponseRedirect(reverse('trainerdex:profile_username', kwargs={'username':request.user.trainer.username}))
         logger.info(form.cleaned_data)
         logger.error(form.errors)
     form.fields['update_time'].widget = forms.HiddenInput()

trainerdex/settings.py
@@ -243,7 +243,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-LOGIN_REDIRECT_URL = 'trainerdex_web:profile'
+LOGIN_REDIRECT_URL = 'trainerdex:profile'
 SOCIALACCOUNT_AUTO_SIGNUP = False
 SOCIALACCOUNT_PROVIDERS = {
     'reddit': {

trainerdex/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^accounts\/profile/first_update\/?$', SetUpProfileViewStep3, name='profile_first_post'),
     url(r'^accounts\/', include('allauth.urls')),
     url(r'^tools\/rosetta\/', include('rosetta.urls')),
-    url(r'^$', RedirectView.as_view(pattern_name='trainerdex_web:leaderboard', permanent=True), name='home'),
+    url(r'^$', RedirectView.as_view(pattern_name='trainerdex:leaderboard', permanent=True), name='home'),
     url(r'^help\/', include('support.urls')),
     url('', include('trainer.urls')),
 ]

website/templates/base.html
@@ -84,8 +84,8 @@
           <div class="trainerdex-header-navigation-container">
             <nav class="trainerdex-header-navigation mdl-navigation">
               {%if user.is_authenticated %}
-              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'trainerdex_web:profile' %}">{% user_display user %}</a>{% else %}<a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'help:about' %}">{% trans "About Us" %}</a>{% endif %}
-              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'trainerdex_web:leaderboard' %}">{% trans "Leaderboard" %}</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'trainerdex:profile' %}">{% user_display user %}</a>{% else %}<a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'help:about' %}">{% trans "About Us" %}</a>{% endif %}
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="{% url 'trainerdex:leaderboard' %}">{% trans "Leaderboard" %}</a>
             </nav>
           </div>
           <div class="trainerdex-mobile-title mdl-layout-title">
@@ -108,7 +108,7 @@
       <div class="trainerdex-drawer mdl-layout__drawer mdl-color--primary-dark mdl-color-text--primary-contrast">
         <header class="trainerdex-drawer-header">
           {% load account %}
-          <a href="{% url 'trainerdex_web:profile' %}">{#<img src="{% get_static_prefix %}{{user.trainer.faction.image|default_if_none: 'img/generic_avatar_male.png'}}" class="trainerdex-avatar">#}</a>
+          <a href="{% url 'trainerdex:profile' %}">{#<img src="{% get_static_prefix %}{{user.trainer.faction.image|default_if_none: 'img/generic_avatar_male.png'}}" class="trainerdex-avatar">#}</a>
           <div class="trainerdex-avatar-dropdown">
             <span>{%if user.is_authenticated %}{% user_display user %}{% else %}{% trans "Red" context "This is the display name for a user not logged in. Use local references if you like. In English, I've gone for Red, the protagonist in the Gen 1 games." %}{% endif %}</span>
             <div class="mdl-layout-spacer"></div>
@@ -129,8 +129,8 @@
         </header>
         <nav class="trainerdex-navigation mdl-navigation mdl-color--primary mdl-color-text--primary-contrast">
           {%if user.is_authenticated %}
-          <a class="mdl-navigation__link" href="{% url 'trainerdex_web:profile' %}"><i class="material-icons">person</i>{% trans "My Profile" %}</a>{% else %}<a class="mdl-navigation__link mdl-color-text--primary-contrast" href="{% url 'home' %}"><i class="material-icons">home</i>{% trans "Home" %}</a>{% endif %}
-          <a class="mdl-navigation__link" href="{% url 'trainerdex_web:leaderboard' %}"><i class="material-icons" role="presentation">assessment</i>{% trans "Leaderboard" %}</a>
+          <a class="mdl-navigation__link" href="{% url 'trainerdex:profile' %}"><i class="material-icons">person</i>{% trans "My Profile" %}</a>{% else %}<a class="mdl-navigation__link mdl-color-text--primary-contrast" href="{% url 'home' %}"><i class="material-icons">home</i>{% trans "Home" %}</a>{% endif %}
+          <a class="mdl-navigation__link" href="{% url 'trainerdex:leaderboard' %}"><i class="material-icons" role="presentation">assessment</i>{% trans "Leaderboard" %}</a>
           <div class="mdl-layout-spacer"></div>
           <a class="mdl-navigation__link mdl-button--disabled"><i class="material-icons" role="presentation">help_outline</i><span class="visuallyhidden">{% trans 'Help' %}</span></a>
         </nav>
@@ -176,7 +176,7 @@
     
     {% block update_stats_button %}
     {% if user.is_authenticated %}
-    <a id="trainerdex__update-button" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored  mdl-shadow--2dp" href="{% url 'trainerdex_web:update_stats' %}"><i class="material-icons">add</i></a>
+    <a id="trainerdex__update-button" class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored  mdl-shadow--2dp" href="{% url 'trainerdex:update_stats' %}"><i class="material-icons">add</i></a>
     {% endif %}
     {% endblock %}
     