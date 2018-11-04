5425206da702337dae9f857c08ec25614e866406
Jay Turner <jay@trainerdex.co.uk>
2018-11-04 14:11:24 +0000

    Migrations: Squash Migrations
    Squash Migrations and Removed Unused Elements

--- support/API/errors.md
+++
@@ -1,2 +0,0 @@
-# Error 1
-This is returned when a TrainerDex profile is inaccessable due to a trainer disactivating their account or requesting to be removed from search
--- trainer/admin.py
+++ trainer/admin.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from trainer.models import *
 
-admin.site.register(FactionLeader)
-
 @admin.register(Faction)
 class FactionAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("name_en",)}
