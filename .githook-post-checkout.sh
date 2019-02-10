6dba5399ae9f2158a7fc585be7f5aa6c42cc2f41
Jay Turner <jay@trainerdex.co.uk>
2019-02-10 19:52:17 +0000

    Explode functions
    Make things a bit more readable

--- .githook-post-checkout.sh
+++ .githook-post-checkout.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 #cp .githook-post-checkout.sh .git/hooks/post-checkout
 echo "[post-checkout hook: $1]"
 
@@ -10,10 +10,6 @@ check_run() {
 
 check_run "core/static/package.json" "(echo -e '\033[0;31mnode_modules changed\e[0m'; cd website/static; npm update --verbose)"
 check_run "requirements.txt" "(echo -e '\033[0;31mpython requirements.txt changed\e[0m'; source env/bin/activate; pip install -r requirements.txt)"
-if ! type python3.6; then
-  check_run "(static)|(requirements.txt)" "(echo -e '\033[0;31mstatic files changed\e[0m'; source env/bin/activate; python manage.py collectstatic --clear --noinput)"
-else
-  check_run "(static)|(requirements.txt)" "(echo -e '\033[0;31mstatic files changed\e[0m'; source env/bin/activate; python3.6 manage.py collectstatic --clear --noinput)"
-fi
+check_run "(static)|(requirements.txt)" "(echo -e '\033[0;31mstatic files changed\e[0m'; source env/bin/activate; python manage.py collectstatic --clear --noinput)"
 
 exit 0
