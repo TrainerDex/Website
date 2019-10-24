43234c217ecfa1f0e6f534e422a43578f08e3a24
Jay Turner <jay@trainerdex.co.uk>
2019-10-24 15:52:29 +0100

    Initial Steps of Bot

---
+++ discordbot/management/commands/runbot.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+import discord
+from discordbot import __version__ as VERSION
+from discord.ext.commands import Bot
+from discordbot.welcome import Welcomer
+
+class Command(BaseCommand):
+    help = 'Starts a Discord.py bot'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('token', nargs=1, type=str)
+    
+    def handle(self, *args, **options):
+        print('Building bot verion', VERSION)
+        bot = Bot(
+        command_prefix='.',
+        case_insensitive=True,
+        description="Welcome to TrainerDex. I am the rewrite of the new bot. Call me discordbot2 for short. The name isn't very creative but what are you going to do.",
+        owner_id=319792326958514176,
+        activity=discord.Game(VERSION)
+        )
+        print('Running verion', VERSION)
+        print('Initiating Cogs')
+        bot.add_cog(Welcomer(bot))
+        print('Starting Bot')
+        bot.run(options['token'][0])
---
+++ discordbot/welcome.py
@@ -0,0 +1,49 @@
+from discord.ext import commands
+from core.models import DiscordUser, DiscordGuild
+
+class Welcomer(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        print('Welcomer initiated')
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        guilddb = DiscordGuild.objects.get_or_create(id=member.guild.id)[0]
+        channel = member.guild.system_channel
+        if channel is not None:
+            try:
+                trainer = DiscordUser.objects.get(uid=str(member.id)).user.trainer
+                if guilddb.settings_welcomer_message_existing:
+                    await channel.send(guilddb.settings_welcomer_message_existing.format(trainer=trainer, guild=member.guild, member=member))
+                else:
+                    await channel.send("Welcome {trainer.nickname} to {guild.name}. I've found you in our database.".format(trainer=trainer, guild=member.guild, member=member))
+            except DiscordUser.DoesNotExist:
+                if guilddb.settings_welcomer_message_new:
+                    await channel.send(guilddb.settings_welcomer_message_existing.format(trainer=trainer, guild=member.guild, member=member))
+                else:
+                    await channel.send("Welcome {member} to {guild.name}. Are you new?".format(guild=member.guild, member=member))
+    
+    @commands.command()
+    async def simulate_join(self, ctx):
+        """Simulates on_member_join"""
+        await self.on_member_join(ctx.author)
+    
+    @commands.command()
+    async def set_welcome_message_new(self, ctx, *, msg: str):
+        """Sets the welcome message"""
+        if msg=='none':
+            msg=None
+        guilddb = DiscordGuild.objects.get_or_create(id=ctx.message.channel.guild.id)[0]
+        guilddb.settings_welcomer_message_new = msg
+        guilddb.save()
+        await ctx.send(f"Welcome message for new users set to '{guilddb.settings_welcomer_message_new}' for this server.")
+    
+    @commands.command()
+    async def set_welcome_message_existing(self, ctx, *, msg: str):
+        """Sets the welcome message"""
+        if msg=='none':
+            msg=None
+        guilddb = DiscordGuild.objects.get_or_create(id=ctx.message.channel.guild.id)[0]
+        guilddb.settings_welcomer_message_existing = msg
+        guilddb.save()
+        await ctx.send(f"Welcome message for existing users set to '{guilddb.settings_welcomer_message_existing}' for this server.")
