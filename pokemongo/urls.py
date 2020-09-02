7bee946c1e6dd11d1b6bba20fd38fdfbcbbab6c5
Jay Turner <jay@trainerdex.co.uk>
2020-09-02 10:25:44 +0100

    Changes to URLS

--- pokemongo/urls.py
+++ pokemongo/urls.py
@@ -1,51 +1,25 @@
 ﻿from django.conf.urls import url
-from pokemongo.views import (
-    LeaderboardView,
-    CreateUpdateView,
-    TrainerRedirectorView,
-)
+from django.views.generic import RedirectView
+from pokemongo.views import LeaderboardView, CreateUpdateView, TrainerRedirectorView
 
 app_name = "trainerdex"
 
 urlpatterns = [
     url(r"^leaderboard\/?$", LeaderboardView, name="leaderboard"),
-    url(
-        r"^leaderboard\/continent\/(?P<continent>[\w]+)\/?$",
-        LeaderboardView,
-        name="leaderboard",
-    ),
-    url(
-        r"^leaderboard\/country\/(?P<country>[\w]+)\/?$",
-        LeaderboardView,
-        name="leaderboard",
-    ),
-    url(
-        r"^leaderboard\/country\/(?P<country>[\w]+)\/(?P<region>[\w\d]+)\/?$",
-        LeaderboardView,
-        name="leaderboard",
-    ),
+    url(r"^leaderboard\/country\/(?P<country>[\w]+)\/?$", LeaderboardView, name="leaderboard"),
     url(
         r"^leaderboard\/community\/(?P<community>[\w\d]+)\/?$",
         LeaderboardView,
         name="leaderboard",
     ),
     url(r"^profile\/?$", TrainerRedirectorView, name="profile"),
-    url(
-        r"^profile\/username\/(?P<nickname>[\w\d]{3,15})\/?$",
-        TrainerRedirectorView,
-        name="profile",
-    ),
-    url(
-        r"^profile\/nickname\/(?P<nickname>[\w\d]{3,15})\/?$",
-        TrainerRedirectorView,
-        name="profile",
-    ),
     url(r"^profile\/id\/(?P<id>[\d]+)\/?$", TrainerRedirectorView, name="profile"),
-    url(r"^tools\/update_stats\/?$", CreateUpdateView, name="update_stats"),
+    url(r"^new\/?$", CreateUpdateView, name="update_stats"),
+    url(
+        r"^tools\/update_stats\/?$", RedirectView.pattern_name(url="update_stats", permanent=True)
+    ),
     url(r"^(?P<nickname>[A-Za-z0-9]{3,15})\/?$", TrainerRedirectorView),
     url(
-        r"^u\/(?P<nickname>[A-Za-z0-9]{3,15})\/?$",
-        TrainerRedirectorView,
-        name="profile_nickname",
+        r"^u\/(?P<nickname>[A-Za-z0-9]{3,15})\/?$", TrainerRedirectorView, name="profile_nickname",
     ),
 ]
