6a8f221b6ad20714e352dfe57b8afc96a15a13a0
Jay Turner <jay@trainerdex.co.uk>
2020-10-10 14:33:09 +0100

    Add version count

---
+++ trainerdex/__init__.py
@@ -0,0 +1,17 @@
+import subprocess
+
+commit_count = (
+    subprocess.run(["git", "rev-list", "HEAD", "--count"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+commit_sha = (
+    subprocess.run(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+
+__version__ = "0.{commit_count}-{commit_sha}".format(
+    commit_count=commit_count, commit_sha=commit_sha
+)
--- trainerdex/wsgi.py
+++ trainerdex/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from trainerdex import __version__
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trainerdex.settings")
 
 application = get_wsgi_application()
+
+print("TrainerDex Version:", __version__)
