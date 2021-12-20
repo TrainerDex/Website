e6fd9efc161b83d3d0c43530b4251dc729c67df2
Jay <jaynicholasturner@gmail.com>
2021-12-20 23:25:46 +0000

    Add db sslmode setting

--- zapdos/config/settings.py
+++ zapdos/config/settings.py
@@ -125,6 +125,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
+        "OPTIONS": {"sslmode": os.environ.get("SQL_SSLMODE", "disable")},
     }
 }
 
