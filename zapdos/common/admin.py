a3c1f6c991e4b64068a4a0f18a3424b6c08229f4
Jay <jaynicholasturner@gmail.com>
2021-12-28 16:00:55 +0000

    More Admin Stuff

--- zapdos/common/admin.py
+++ zapdos/common/admin.py
@@ -1,7 +1,9 @@
+from typing import Any, Dict, List, Optional, Tuple
 from allauth.socialaccount.admin import SocialAccountAdmin
 from allauth.socialaccount.models import SocialAccount
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.http.request import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
 from common.models import FeedPost, User, UsernameHistory
@@ -18,6 +20,77 @@ class ZapdosSocialAccountAdmin(SocialAccountAdmin):
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "timezone",
+                    "country",
+                )
+            },
+        ),
+        (
+            _("Pokémon Go"),
+            {
+                "fields": (
+                    "trainer_code",
+                    "daily_goal",
+                    "total_goal",
+                    "pogo_date_created",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_public",
+                    "is_perma_banned",
+                    "is_verified",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (
+            _("Important dates"),
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+        (
+            _("Pokémon Go"),
+            {
+                "fields": (
+                    "trainer_code",
+                    "daily_goal",
+                    "total_goal",
+                    "pogo_date_created",
+                )
+            },
+        ),
+    )
+
     list_display = (
         "username",
         "faction",
