e97f3a55e1f6e1c4c3838dcd34eda931542fc8c9
Jay Turner <jay@trainerdex.app>
2022-03-16 21:12:09 +0000

    Remove bad config

--- trainerdex/settings.py
+++ trainerdex/settings.py
@@ -152,8 +152,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
 # CORS
 # https://github.com/ottoyiu/django-cors-headers
 
