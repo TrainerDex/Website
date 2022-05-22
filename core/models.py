b97a978f5e033da2e00ab1b45d7ae91c3536493c
Jay Turner <jaynicholasturner@gmail.com>
2022-05-22 15:52:19 +0100

    Add Service models

--- core/models.py
+++ core/models.py
@@ -16,6 +16,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from pytz import common_timezones
 
+from config.abstract_models import PublicModel
+
 logger = logging.getLogger("django.trainerdex")
 
 
@@ -587,3 +589,40 @@ class DiscordGuildMembership(models.Model):
         verbose_name = _("Discord Member")
         verbose_name_plural = _("Discord Members")
         unique_together = (("guild", "user"),)
+
+
+class Service(PublicModel):
+    name = models.CharField(verbose_name=_("name"), max_length=32)
+    description = models.TextField(verbose_name=_("description"), blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_status(self) -> ServiceStatus:
+        return self.statuses.latest("created_at")
+
+
+class StatusChoices(models.TextChoices):
+    UP = "UP", _("Up")
+    DOWN = "DOWN", _("Down")
+    UNSTABLE = "UNSTABLE", _("Unstable")
+    MAINTENANCE = "MAINTENANCE", _("Scheduled Maintenance")
+    UNKNOWN = "UNKNOWN", _("Unknown")
+
+
+class ServiceStatus(PublicModel):
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        verbose_name=_("service"),
+        related_name="statuses",
+    )
+    status = models.CharField(
+        choices=StatusChoices.choices,
+        max_length=len(max(StatusChoices.values, key=len)),
+        verbose_name=_("status"),
+    )
+    reason = models.TextField(verbose_name=_("reason"), blank=True)
+
+    def __str__(self):
+        return f"{self.service} is {self.status}"
