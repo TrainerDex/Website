d11750e2de5a09bd68235e1096e1e3e820545f04
Jay Turner <jaynicholasturner@gmail.com>
2022-05-28 13:52:30 +0100

    Add Oauth

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount.providers.twitter",
     "widget_tweaks",
     "robots",
+    "oauth2_provider",
 ]
 
 MIDDLEWARE = [
@@ -175,7 +176,10 @@ CSRF_TRUSTED_ORIGINS = ["https://trainerdex.app", "https://beta.trainerdex.app"]
 # http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.TokenAuthentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
+    ),
     "DEFAULT_PERMISSION_CLASSES": ("config.permissions.IsAdminUserOrReadOnly",),
 }
 
