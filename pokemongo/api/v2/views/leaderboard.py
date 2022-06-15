05af2d4493e6586a237dfab49a885e56bfd015a8
Jay Turner <jay@trainerdex.app>
2022-06-16 00:02:40 +0100

    Revert "Leaderboard View start"
    This reverts commit 0e4240dacc5c205026bace789d180ec794d8fa54.

--- pokemongo/api/v2/views/leaderboard.py
+++ pokemongo/api/v2/views/leaderboard.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
-from datetime import date
 
-from attrs import NOTHING
-from attrs import define, field, validators
 from django.db.models import Q
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication
 from rest_framework.authentication import SessionAuthentication
@@ -15,15 +12,12 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 from core.models.discord import DiscordGuild
 from pokemongo.api.v2.handlers.leaderboard import (
     DiscordLeaderboardHandler,
     GlobalLeaderboardHandler,
 )
-from pokemongo.shortcuts import UPDATE_SORTABLE_FIELDS
 
 
 @api_view(["GET"])
@@ -48,35 +42,3 @@ def get_discord_leaderboard(request: Request, guild_id: id) -> Response:
         return Response(DiscordLeaderboardHandler.get_data(request, guild))
 
     raise PermissionDenied
-
-
-@define
-class LeaderboardParams:
-    stat: str = field(default="total_xp", validator=validators.in_(UPDATE_SORTABLE_FIELDS))
-    start_date: date = field(default=NOTHING, converter=date.fromisoformat)
-    end_date: date = field(default=NOTHING, converter=date.fromisoformat)
-    date: date = field(default=NOTHING, converter=date.fromisoformat)
-    mode: str = field(default="snapshot", validator=validators.in_(["snapshot", "gain"]))
-    primary_filter_type: str = field(
-        default="all", validator=validators.in_(["all", "discord", "community", "country"])
-    )
-    primary_filter_value: str = field(default=NOTHING)
-
-
-class LeaderboardView(APIView):
-    authentication_classes = [OAuth2Authentication, SessionAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def parse_query_params(self, request: Request) -> LeaderboardParams:
-        self.parameters = LeaderboardParams(**request.query_params)
-        return self.parameters
-
-    def get(self, request: Request) -> Response:
-        self.parse_query_params(request)
-
-        self.get_trainer_subquery()
-        self.get_subquery()
-        self.get_query()
-
-        data = self.serialize_data()
-        return Response(data)
