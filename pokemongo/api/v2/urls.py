c6f24a80a1868f318499fa388ba8ca4299688656
Jay Turner <jay@trainerdex.app>
2022-06-15 23:01:34 +0100

    URLs / APIs plan

--- pokemongo/api/v2/urls.py
+++ pokemongo/api/v2/urls.py
@@ -13,3 +13,25 @@ urlpatterns = [
     path("discord-leaderboard/<int:guild_id>/", get_discord_leaderboard),
     path("discord-diffboard/<int:guild_id>/", get_discord_diffboard),
 ]
+
+
+"""
+URLs for the v2 API.
+
+
+/api/v2/leaderboard
+
+Do we want seperate endpoints for global and discord leaderboards?
+Do we want the diffboard to be a seperate endpoint?
+URLs can be a bit long and confusing for these endpoints. Perhaps one endpoint controlled with params?
+
+/api/v2/trainers/@me
+/api/v2/trainers/<trainer_name>
+/api/v2/trainers/<trainer_name>/nicknames
+/api/v2/trainers/<trainer_name>/posts (If we introduce posts)
+/api/v2/trainers/<trainer_name>/social
+/api/v2/trainers/<trainer_name>/stats
+/api/v2/trainers/<trainer_name>/stats/latest
+
+
+"""
