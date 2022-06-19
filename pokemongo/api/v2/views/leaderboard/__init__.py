279e1c253c35d674f54af60e84715577e0ed9484
Jay Turner <jay@trainerdex.app>
2022-06-19 14:36:55 +0100

    Resolve Gains on viewfinder

--- pokemongo/api/v2/views/leaderboard/__init__.py
+++ pokemongo/api/v2/views/leaderboard/__init__.py
@@ -1,3 +1,5 @@
 from .gain import DiscordGainLeaderboardView, GainLeaderboardView
+from .gain.interface import iGainLeaderboardView
 from .snapshot import DiscordSnapshotLeaderboardView, SnapshotLeaderboardView
+from .snapshot.interface import iSnapshotLeaderboardView
 from .view_finder import LeaderboardViewFinder
