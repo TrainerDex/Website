c2bbd23c1cc70af9af655d081d414c57866bdee3
Jay Turner <jaynicholasturner@gmail.com>
2022-06-25 00:03:04 +0100

    Basic leaderboard table

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,19 @@
 <script lang="ts">
-  import logo from "./assets/svelte.png";
-  import Counter from "./lib/Leaderboard.svelte";
+  // import logo from "./assets/svelte.png";
+  import Leaderboard from "./lib/Leaderboard.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+  <!-- <img src={logo} alt="Svelte Logo" /> -->
+  <h1>TrainerDex</h1>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Leaderboard />
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
--- src/lib/Leaderboard.svelte
+++ src/lib/Leaderboard.svelte
@@ -2,65 +2,94 @@
   import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
   import LinearProgress from "@smui/linear-progress";
 
+  type SnapshotLeaderboardAggregation = {
+    average: number;
+    min: number;
+    max: number;
+    sum: number;
+  };
+
   type SnapshotLeaderboardEntry = {
     rank: number;
     username: string;
     faction: number;
     value: number;
     trainer_uuid: string;
+    entry_uuid: string;
+    entry_datetime: Date;
   };
 
-  let items: SnapshotLeaderboardEntry[] = [];
+  type SnapshotLeaderboard = {
+    count: number | null;
+    next: URL | null;
+    previous: URL | null;
+    generated_datetime: Date;
+    datetime: Date;
+    title: string;
+    stat: string;
+    aggregations: SnapshotLeaderboardAggregation;
+    entries: SnapshotLeaderboardEntry[];
+  };
+
+  let leaderboard: SnapshotLeaderboard | null = null;
   let loaded = false;
 
-  loadThings(false);
-
-  function loadThings(wait: boolean) {
+  async function loadLeaderboard(
+    stat: string = "total_xp"
+  ): Promise<SnapshotLeaderboard> {
     if (typeof fetch !== "undefined") {
-      loaded = false;
-
-      fetch(
-        "https://gist.githubusercontent.com/hperrin/e24a4ebd9afdf2a8c283338ae5160a62/raw/dcbf8e6382db49b0dcab70b22f56b1cc444f26d4/users.json"
-      )
+      return fetch(`https://trainerdex.app/api/v2/leaderboard/?stat=${stat}`)
         .then((response) => response.json())
-        .then((json) =>
-          setTimeout(
-            () => {
-              items = json;
-              loaded = true;
-            },
-            // Simulate a long load time.
-            wait ? 2000 : 0
-          )
-        );
+        .then((json) => json);
     }
   }
 </script>
 
-<DataTable table$aria-label="User list" style="width: 100%;">
-  <Head>
-    <Row>
-      <Cell numeric>ID</Cell>
-      <Cell style="width: 100%;">Name</Cell>
-      <Cell>Username</Cell>
-      <Cell>Email</Cell>
-    </Row>
-  </Head>
-  <Body>
-    {#each items as item (item.id)}
+{#await loadLeaderboard()}
+  <DataTable table$aria-label="Leaderboard" style="width: 100%;">
+    <Head>
       <Row>
-        <Cell numeric>{item.id}</Cell>
-        <Cell>{item.name}</Cell>
-        <Cell>{item.username}</Cell>
-        <Cell>{item.email}</Cell>
+        <Cell numeric>Rank</Cell>
+        <Cell style="width: 100%;">Name</Cell>
+        <Cell>Stat</Cell>
+        <Cell>Submitted at</Cell>
       </Row>
-    {/each}
-  </Body>
+    </Head>
+    <Body />
 
-  <LinearProgress
-    indeterminate
-    bind:closed={loaded}
-    aria-label="Data is being loaded..."
-    slot="progress"
-  />
-</DataTable>
+    <LinearProgress
+      indeterminate
+      bind:closed={loaded}
+      aria-label="Data is being loaded..."
+      slot="progress"
+    />
+  </DataTable>
+{:then leaderboard}
+  <DataTable table$aria-label="Leaderboard" style="width: 100%;">
+    <Head>
+      <Row>
+        <Cell numeric>Rank</Cell>
+        <Cell style="width: 100%;">Name</Cell>
+        <Cell>{leaderboard.stat}</Cell>
+        <Cell>Submitted at</Cell>
+      </Row>
+    </Head>
+    <Body>
+      {#each leaderboard.entries as entry (entry.trainer_uuid)}
+        <Row>
+          <Cell numeric>{entry.rank}</Cell>
+          <Cell>{entry.username}</Cell>
+          <Cell>{entry.value}</Cell>
+          <Cell>{entry.entry_datetime}</Cell>
+        </Row>
+      {/each}
+    </Body>
+
+    <LinearProgress
+      indeterminate
+      bind:closed={loaded}
+      aria-label="Data is being loaded..."
+      slot="progress"
+    />
+  </DataTable>
+{/await}
