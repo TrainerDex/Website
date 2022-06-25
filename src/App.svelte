815265bd0ec452e3d91dfe958986ff63395fe334
Jay Turner <jaynicholasturner@gmail.com>
2022-06-25 16:54:29 +0100

    Remove unused code

--- src/App.svelte
+++ src/App.svelte
@@ -50,42 +50,6 @@
   }
 
   :root {
-    font-family: "Raleway", "Roboto", "Segoe UI", Roboto, Oxygen, Ubuntu,
-      Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+    font-family: "Raleway", Roboto, Oxygen, Ubuntu, "Open Sans", sans-serif;
   }
 </style>
