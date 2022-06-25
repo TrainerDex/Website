73e4179e5f310b257a205650aea7f9e9189f4a5c
Jay Turner <jaynicholasturner@gmail.com>
2022-06-25 14:48:48 +0100

    Add Pagination and Stat select box

--- src/lib/Leaderboard.svelte
+++ src/lib/Leaderboard.svelte
@@ -1,33 +1,39 @@
 <script lang="ts">
   import DataTable, { Head, Body, Row, Cell } from "@smui/data-table";
-  import LinearProgress from "@smui/linear-progress";
-  import Card, {
-    Content,
-    PrimaryAction,
-    Actions,
-    ActionButtons,
-    ActionIcons,
-  } from "@smui/card";
-  import Button, { Label } from "@smui/button";
   import Select, { Option } from "@smui/select";
-  import IconButton, { Icon } from "@smui/icon-button";
+  import IconButton from "@smui/icon-button";
+  import { Label } from "@smui/common";
+  import LinearProgress from "@smui/linear-progress";
+  import Card, { Content } from "@smui/card";
   import {
     getStatByParameter,
-    StatMeta,
     TotalXP,
     formatValue,
     listOfStats,
   } from "../models/stats";
   import moment from "moment";
   import type { SnapshotLeaderboard } from "src/models/leaderboard";
+  import Pagination from "./Pagination.svelte";
 
   let statString = TotalXP.parameter;
   $: stat = getStatByParameter(statString);
 
-  $: loadLeaderboard = async (): Promise<SnapshotLeaderboard> => {
+  let leaderboard: SnapshotLeaderboard;
+
+  $: loaded = leaderboard?.count ?? false;
+
+  let rowsPerPage = 25;
+  $: rowOffset = stat ? 0 : 0; // This resets rowOffset to 0 when stat is changed.
+
+  $: maximumRowOffset = loaded ? Math.ceil(leaderboard.count - rowsPerPage) : 0;
+
+  $: hasNextPage = rowOffset < maximumRowOffset;
+  $: hasPreviousPage = rowOffset > 0;
+
+  $: loadPage = async (): Promise<void> => {
     if (typeof fetch !== "undefined") {
-      return fetch(
-        `https://trainerdex.app/api/v2/leaderboard/?stat=${stat.parameter}`
+      leaderboard = await fetch(
+        `https://trainerdex.app/api/v2/leaderboard/?stat=${stat.parameter}&limit=${rowsPerPage}&offset=${rowOffset}`
       )
         .then((response) => response.json())
         .then((json) => json);
@@ -35,72 +41,83 @@
   };
 </script>
 
-<div class="card-display">
-  <div class="card-container">
-    <Card>
-      <Content>
-        {#await loadLeaderboard()}
-          <DataTable table$aria-label="Leaderboard" style="width: 100%;">
-            <Head>
-              <Row>
-                <Cell numeric>Rank</Cell>
-                <Cell style="width: 100%;">Name</Cell>
-                <Cell>Stat</Cell>
-                <Cell>Submitted at</Cell>
-              </Row>
-            </Head>
-            <Body />
+{#await loadPage()}
+  <LinearProgress
+    indeterminate
+    aria-label="Data is being loaded..."
+    slot="progress"
+  />
+{:then}
+  <DataTable table$aria-label="Leaderboard" style="width: 100%;">
+    <Head>
+      <Row>
+        <Cell numeric>Rank</Cell>
+        <Cell style="width: 100%;">Name</Cell>
+        <Cell>{getStatByParameter(leaderboard.stat).name}</Cell>
+        <Cell>Submitted at</Cell>
+      </Row>
+    </Head>
+    <Body>
+      {#each leaderboard.entries as entry (entry.trainer_uuid)}
+        <Row>
+          <Cell numeric>{entry.rank}</Cell>
+          <Cell>{entry.username}</Cell>
+          <Cell>{formatValue(entry.value, leaderboard.stat)}</Cell>
+          <Cell>{moment(entry.entry_datetime).format("ll")}</Cell>
+        </Row>
+      {/each}
+    </Body>
+    <Pagination slot="paginate">
+      <svelte:fragment slot="stat">
+        <Label>Selected Stat</Label>
+        <Select variant="outlined" bind:value={statString} noLabel>
+          {#each listOfStats as stat}
+            <Option value={stat.parameter}>{stat.name}</Option>
+          {/each}
+        </Select>
+      </svelte:fragment>
 
-            <LinearProgress
-              indeterminate
-              aria-label="Data is being loaded..."
-              slot="progress"
-            />
-          </DataTable>
-        {:then leaderboard}
-          <DataTable table$aria-label="Leaderboard" style="width: 100%;">
-            <Head>
-              <Row>
-                <Cell numeric>Rank</Cell>
-                <Cell style="width: 100%;">Name</Cell>
-                <Cell>{getStatByParameter(leaderboard.stat).name}</Cell>
-                <Cell>Submitted at</Cell>
-              </Row>
-            </Head>
-            <Body>
-              {#each leaderboard.entries as entry (entry.trainer_uuid)}
-                <Row>
-                  <Cell numeric>{entry.rank}</Cell>
-                  <Cell>{entry.username}</Cell>
-                  <Cell>{formatValue(entry.value, leaderboard.stat)}</Cell>
-                  <Cell>{moment(entry.entry_datetime).format("ll")}</Cell>
-                </Row>
-              {/each}
-            </Body>
-          </DataTable>
-        {/await}
-      </Content>
-      <Actions>
-        <div class="columns margins" style="justify-content: flex-start;">
-          <Select bind:value={statString} label="Stat">
-            {#each listOfStats as stat}
-              <Option value={stat.parameter}>{stat.name}</Option>
-            {/each}
-          </Select>
-        </div>
-        <IconButton
-          toggle
-          aria-label="Add to favorites"
-          title="Add to favorites"
-        >
-          <Icon class="material-icons" on>favorite</Icon>
-          <Icon class="material-icons">favorite_border</Icon>
-        </IconButton>
-        <IconButton class="material-icons" title="Share">share</IconButton>
-        <IconButton class="material-icons" title="More options"
-          >more_vert</IconButton
-        >
-      </Actions>
-    </Card>
-  </div>
-</div>
+      <svelte:fragment slot="rowsPerPage">
+        <Label>Rows Per Page</Label>
+        <Select variant="outlined" bind:value={rowsPerPage} noLabel>
+          <Option value={10}>10</Option>
+          <Option value={25}>25</Option>
+          <Option value={100}>100</Option>
+        </Select>
+      </svelte:fragment>
+      <svelte:fragment slot="total">
+        {rowOffset + 1}-{rowOffset + rowsPerPage} of {leaderboard.count}
+      </svelte:fragment>
+
+      <IconButton
+        class="material-icons"
+        action="first-page"
+        title="First page"
+        on:click={() => (rowOffset = 0)}
+        disabled={!hasPreviousPage}>first_page</IconButton
+      >
+      <IconButton
+        class="material-icons"
+        action="prev-page"
+        title="Prev page"
+        on:click={() => (rowOffset = Math.max(rowOffset - rowsPerPage, 0))}
+        disabled={!hasPreviousPage}>chevron_left</IconButton
+      >
+      <IconButton
+        class="material-icons"
+        action="next-page"
+        title="Next page"
+        on:click={() =>
+          (rowOffset = Math.min(rowOffset + rowsPerPage, maximumRowOffset))}
+        disabled={!hasNextPage}>chevron_right</IconButton
+      >
+      <IconButton
+        class="material-icons"
+        action="last-page"
+        title="Last page"
+        on:click={() => (rowOffset = maximumRowOffset)}
+        disabled={!hasNextPage}>last_page</IconButton
+      >
+    </Pagination>
+  </DataTable>
+{/await}
