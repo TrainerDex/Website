c6de7d0acdfc0cd40f4b64a230f174851f15c649
Jay Turner <jay@trainerdex.app>
2022-07-15 22:58:51 +0100

    Set up rollup

---
+++ frontend/src/routes/Leaderboard.ts
@@ -0,0 +1,8 @@
+import App from './Leaderboard.svelte';
+
+const app = new App({
+	target: document.getElementById('app-target'),
+	props: JSON.parse(document.getElementById('app-props').textContent)
+});
+
+export default app;
