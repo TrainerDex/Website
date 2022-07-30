c0e209cf06fc1c345eab6d168081067d30c8085d
Jay Turner <jay@trainerdex.app>
2022-07-30 19:02:10 +0100

    Add worker count var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 pipenv run python manage.py migrate --no-color --noinput -v 3
 
-pipenv run gunicorn --worker-tmp-dir /dev/shm config.wsgi
+pipenv run gunicorn --worker-tmp-dir /dev/shm config.wsgi -w $WORKER_COUNT
