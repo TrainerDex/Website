ae316d317ed37b90c92cf4afb413d5f52cfa87de
Jay Turner <jay@trainerdex.app>
2022-07-31 22:18:40 +0100

    Add Template Matching and tighter cropping rules, changed output shape

--- ocr/views.py
+++ ocr/views.py
@@ -1,19 +1,32 @@
 import re
 from collections import defaultdict
+from dataclasses import dataclass
 from decimal import Decimal
 from difflib import SequenceMatcher
-from typing import Iterator, List, NamedTuple
+from enum import Enum
+from math import sqrt
+from typing import Iterator, List, NamedTuple, Optional
 
+import cv2 as cv
+import numpy as np
 import pytesseract
+from django.contrib.staticfiles import finders
 from django.core.files.uploadedfile import UploadedFile
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication
-from PIL import Image
+from PIL import Image, ImageDraw
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.parsers import FileUploadParser
 from rest_framework.permissions import IsAdminUser
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+class BoundingBox(NamedTuple):
+    left: int
+    top: int
+    right: int
+    bottom: int
 
 
 class Word(NamedTuple):
@@ -83,6 +96,21 @@ class Line(NamedTuple):
         return f"[{self.page_num}:{self.block_num}:{self.par_num}:{self.line_num}] {self.bounding_box}: {self.text}"
 
 
+@dataclass
+class FoundTarget:
+    left_line: Optional[Line] = None
+    left_image: Optional[Image.Image] = None
+    right_image: Optional[Image.Image] = None
+    found_value: Optional[str] = None
+
+
+class SearchTerms(Enum):
+    DISTANCE_TRAVELED = "Distance"
+    POKEMON_CAUGHT = "Pokémon"
+    POKESTOPS_SPUN = "PokéStops"
+    TOTAL_XP = "Total"
+
+
 class ActivityViewOCR(APIView):
     parser_classes = [FileUploadParser]
     # authentication_classes = [TokenAuthentication, OAuth2Authentication]
@@ -116,12 +144,82 @@ class ActivityViewOCR(APIView):
         for key, line in lines.items():
             yield Line(*key, words=line)
 
-    def put(self, request: Request):
-        file_object: UploadedFile = request.data["file"]
-        Image.open(file_object).verify()
+    @staticmethod
+    def pillow_to_cv(image: Image.Image) -> np.ndarray[int, np.dtype[np.generic]]:
+        return cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
 
-        image = Image.open(file_object)
+    def detect_close_button(self, image: Image.Image) -> BoundingBox:
+        img = self.pillow_to_cv(image)
+        template_path = finders.find("img/btn_close_normal_dark.png")
+        template = self.pillow_to_cv(Image.open(template_path))
 
+        w, h, *_ = template.shape
+
+        method = cv.TM_SQDIFF_NORMED
+        # Apply template Matching
+
+        res = cv.matchTemplate(img, template, method)
+        _, _, min_loc, _ = cv.minMaxLoc(res)
+        top_left = min_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        return BoundingBox(
+            left=top_left[0], top=top_left[1], right=bottom_right[0], bottom=bottom_right[1]
+        )
+
+    @staticmethod
+    def get_distance_between_two_pixels(p1: np.ndarray, p2: np.ndarray) -> float:
+        return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+    def cover_close_button(self, image: Image.Image, close_location: BoundingBox) -> None:
+        middle_pixel = np.array(
+            [
+                (close_location.left + close_location.right) // 2,
+                (close_location.top + close_location.bottom) // 2,
+            ]
+        )
+        top_left_pixel = np.array([close_location.left, close_location.top])
+
+        radius = self.get_distance_between_two_pixels(middle_pixel, top_left_pixel)
+
+        new_bounding_box = BoundingBox(
+            left=middle_pixel[0] - radius,
+            top=middle_pixel[1] - radius,
+            right=middle_pixel[0] + radius,
+            bottom=middle_pixel[1] + radius,
+        )
+
+        draw = ImageDraw.Draw(image)
+        draw.ellipse(
+            [
+                (new_bounding_box.left, new_bounding_box.top),
+                (new_bounding_box.right, new_bounding_box.bottom),
+            ],
+            fill="white",
+            outline="white",
+        )
+
+    @staticmethod
+    def process_km_text(s: str) -> Decimal:
+        if s is None:
+            return None
+
+        # Distance is in the form of "69,023.2" or "69.023,2". We need to convert it to "69023.2"
+        s = s.strip()
+        separators = re.sub("[0-9]", "", s)
+        for sep in separators[:-1]:
+            s = s.replace(sep, "")
+        if separators:
+            s = s.replace(separators[-1], ".")
+        return Decimal(s)
+
+    @staticmethod
+    def strip_non_digits(s: str) -> int:
+        if s is None:
+            return None
+        return int(re.sub("[^0-9]", "", s))
+
+    def get_stats_from_activity_section(self, image: Image.Image) -> dict:
         image_left = image.crop((0, 0, image.width // 2, image.height))
         image_right = image.crop((image.width // 2, 0, image.width, image.height))
 
@@ -132,87 +230,77 @@ class ActivityViewOCR(APIView):
 
         left_lines = list(self.process_lines(self.process_words(left_data)))
 
-        stats = {}
-        target_words = iter(["Distance", "Pokémon", "PokéStops", "Total"])
+        target_words = iter(
+            [
+                SearchTerms.DISTANCE_TRAVELED,
+                SearchTerms.POKEMON_CAUGHT,
+                SearchTerms.POKESTOPS_SPUN,
+                SearchTerms.TOTAL_XP,
+            ]
+        )
         target_word = next(target_words)
 
-        found_targets: dict[str, Line] = {}
+        found_targets: dict[str, FoundTarget] = defaultdict(FoundTarget)
 
         while True:
             if target_word is None:
                 break
-            # print("Searching for", target_word)
-            for line in left_lines:
+            for found_target in left_lines:
                 if target_word in found_targets:
                     break
-                # print("Processing line:", line)
-                for word in line.words:
+                for word in found_target.words:
                     similarity_index = SequenceMatcher(
                         None,
                         word.text,
                         target_word,
                     ).ratio()
                     if similarity_index > 0.5:
-                        # print(f"Found {target_word} at {line}")
-                        found_targets[target_word] = line
-                        if target_word == "Total":
-                            print(line)
-                            for word in line.words:
-                                print(word)
-                                word.crop(image_left).show()
-
-                                if word.text == "q":
-                                    image.crop(
-                                        (
-                                            word.left,
-                                            word.top,
-                                            word.width + (image.width // 2),
-                                            word.top + word.height,
-                                        )
-                                    ).show()
+                        found_targets[target_word].left_line = found_target
+                        found_targets[target_word].left_image = found_target.crop(image_left)
                         break
-            target_word = next(target_words, None)
 
-        # right_data = pytesseract.image_to_data(
-        #     image_right.convert("RGB"),
-        #     output_type=pytesseract.Output.DICT,
-        # )
-        # right_lines = self.process_lines(self.process_words(left_data))
+            target_word = next(target_words, None)
 
         new_stat_images: dict[str, Image.Image] = {}
         new_stat_images_str: dict[str, str] = {}
 
-        for target, line in found_targets.items():
-            # Crop image to the same vertile space as the line, but the width is the full image width, except the left 64 pixels, to cut out the rubbish, and the right 24 pixels to cut out the border
+        for found_target in found_targets.values():
+            line = found_target.left_line
             line_height = line.bounding_box[3] - line.bounding_box[1]
-            padding = 0  # (line_height // image_right.height) ** 0.05
+            padding = line_height // 3
             top = line.bounding_box[1] - padding
             bottom = line.bounding_box[3] + padding
             left = 64
             right = image_right.width - 24
 
-            new_stat_images[target] = image_right.convert("RGB").crop((left, top, right, bottom))
-            # new_stat_images[target].show()
-            new_stat_images_str[target] = pytesseract.image_to_string(
-                new_stat_images[target]
+            found_target.right_image = image_right.convert("RGB").crop((left, top, right, bottom))
+            found_target.found_value = pytesseract.image_to_string(
+                found_target.right_image
             ).strip()
 
-        print(new_stat_images_str)
+        return {
+            "total_xp": self.strip_non_digits(
+                found_targets.get(SearchTerms.TOTAL_XP).found_value,
+            ),
+            "travel_km": self.process_km_text(
+                found_targets.get(SearchTerms.DISTANCE_TRAVELED).found_value,
+            ),
+            "capture_total": self.strip_non_digits(
+                found_targets.get(SearchTerms.POKEMON_CAUGHT).found_value,
+            ),
+            "pokestops_visited": self.strip_non_digits(
+                found_targets.get(SearchTerms.POKESTOPS_SPUN).found_value,
+            ),
+        }
 
-        return Response(data=new_stat_images_str, status=200)
+    def put(self, request: Request):
+        file_object: UploadedFile = request.data["file"]
+        Image.open(file_object).verify()
 
+        image = Image.open(file_object)
 
-# if search_pattern == SearchPattern.VALUE:
-#     if (line := re.sub(r"[^0-9]", "", word.text)).strip():
-#         if target_word == "Distance":
-#             # Distance is in the form of "69,023.2" or "69.023,2". We need to convert it to "69023.2"
-#             separators = re.sub("[0-9]", "", word.text)
-#             for sep in separators[:-1]:
-#                 line = word.text.replace(sep, "")
-#             if separators:
-#                 line = line.replace(separators[-1], ".")
-#             stats[target_word] = Decimal(line)
-#         else:
-#             stats[target_word] = int(line)
+        self.cover_close_button(image, self.detect_close_button(image))
 
-#         search_pattern = SearchPattern.NONE
+        stats = self.get_stats_from_activity_section(image)
+
+        return Response(data=stats, status=200)
