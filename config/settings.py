8b3c0ee58a78984ca229a9f87fd586d53cfe6c88
Jay Turner <jaynicholasturner@gmail.com>
2023-04-10 11:53:28 +0100

    Chill out silk

--- config/settings.py
+++ config/settings.py
@@ -261,8 +261,15 @@ SILKY_AUTHORISATION = True  # User must have permissions
 SILKY_IGNORE_PATHS = [
     "/api/health/",  # DigitalOcean calls this every 10 seconds, let's exclude that.
     "/favicon.ico",
+    "/api/ocr/activity-view/",
+    "/sitemap.xml",
+    "/api/ocr/activity-view/",
+    "/api/v2/leaderboard/",
 ]
 SILKY_META = True
+SILKY_MAX_REQUEST_BODY_SIZE = -1  # Silk takes anything <0 as no limit
+SILKY_MAX_RESPONSE_BODY_SIZE = 1024  # If response body>1024 bytes, ignore
+SILKY_INTERCEPT_PERCENT = 50  # log only 50% of requests
 
 
 # Default primary key field type
